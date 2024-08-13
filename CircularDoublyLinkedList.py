44f3f324e91ab8221a023bd17f481575bc6bd948
alireza tahami <alireza.thm03@gmail.com>
2024-08-13 17:38:26 +0330

    cleaning up :broom:

--- CircularDoublyLinkedList.py
+++ CircularDoublyLinkedList.py
@@ -9,7 +9,7 @@ class Node:
         return self.data
 
 
-class CircularLinkedList:
+class CircularDoublyLinkedList:
     def __init__ (self):
         self.head = None
         self.tail = None
@@ -87,6 +87,11 @@ class CircularLinkedList:
                     self.head = self.head.next
                     self.tail.next = self.head
                     self.size -= 1
+                elif current == self.tail:
+                    prev.next = self.head
+                    self.tail = prev
+                    self.head.prev = self.tail
+                    self.size -=1 
                 else:
                     prev.next = current.next
                     self.size -=1 
@@ -96,6 +101,41 @@ class CircularLinkedList:
             counter += 1
 
 
+    def insert(self, data, index):
+        index = self.index_handler(index)
+        current = self.head
+        prev = self.head
+        node = Node(data)
+        counter = 0
+        while current == prev or prev != self.tail:
+            if counter == index:
+                if current == self.head:
+                    self.head = node
+                    self.head.next = current
+                    self.head.prev = self.tail
+                    self.tail.next = self.head
+                    self.size += 1
+                    return
+                elif current == self.tail:
+                    node.prev = self.tail
+                    self.tail.next = node
+                    self.tail = node
+                    self.tail.next = self.head
+                    self.head.prev = self.tail
+                    self.size += 1
+                    return
+                else:
+                    prev.next = node 
+                    node.perv = prev
+                    current.prev = node
+                    node.next = current
+                    self.size += 1
+                    return            
+            prev = current
+            current = current.next
+            counter += 1
+
+    
     def display(self):
         if self.size == 0:
             print()
@@ -105,19 +145,20 @@ class CircularLinkedList:
             counter = 0
             while  counter < self.size:
                 if current.next == self.head:
-                    print(current.data)
+                    print(str(current.data))
                 else:
                     print(current.data, end=', ')
                 current = current.next
                 counter += 1
+        print(nums.tail.data)
 
 
 if __name__ == '__main__':
     
     # create a CircularLinkedList object
-    nums = CircularLinkedList()
+    nums = CircularDoublyLinkedList()
     
-    # appending some items to it
+    # appending some item to it
     for i in range(2,10):
         nums.append(i)
     nums.display()
@@ -144,4 +185,17 @@ if __name__ == '__main__':
     
     # deleting at a specific index (intermediate)
     nums.delete_at(1)
+    nums.display()
+    # print(nums.tail.data)
+    
+    # insert at specific index (first)
+    nums.insert(3, 0)
+    nums.display()
+    
+    # insert at specific index (last)
+    nums.insert(6, -1)
+    nums.display()
+    
+    # insert at specific index (intermediate)
+    nums.insert(5, 2)
     nums.display()
