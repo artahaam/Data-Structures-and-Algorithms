0064dd762ee64d29131450e05e826f7e6a11b005
alireza tahami <alireza.thm03@gmail.com>
2024-08-14 17:50:43 +0330

    enqueue

--- Queues.py
+++ Queues.py
@@ -1,13 +1,25 @@
 # list-based Queue
 class ListQueue:
-    def __init__(self):
+    def __init__(self, size):
         self.items = []
         self.front = self.rear = 0
-        self.size = 3
+        self.size = size
+        
+    
+    def enqueue(self, data):
+        if self.size == self.rear:
+            raise Exception("Queue is full")
+        else:
+            self.items.insert(0, data)
+            self.front += 1
+    
+
+        
+
     
     
     
-# Linkedlist based Queue
+# Linkedlist-based Queue
 class Node(object):
     def __init__(self, data=None, next=None, prev=None):
         self.data = data
@@ -20,3 +32,5 @@ class Queue:
         self.head = None
         self.tail = None
         self.count = 0
+        
+# Stack-based Queues
