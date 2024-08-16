0493b072592297bf6b2aa93230e7431f69630f74
alireza tahami <alireza.thm03@gmail.com>
2024-08-16 16:21:08 +0330

    init

---
+++ BinarySearchTree.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.right_child = None
+        self.left_child = None
+
+        
+class Tree:
+    def __init__(self):
+        self.root_node = None
