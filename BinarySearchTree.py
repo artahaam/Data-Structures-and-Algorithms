0fe08506c6f56a2ad13e219e2db62c5bf65899d0
alireza tahami <alireza.thm03@gmail.com>
2024-08-18 11:20:44 +0330

    insert()

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -1,10 +1,57 @@
 class Node:
     def __init__(self, data):
         self.data = data
-        self.right_child = None
-        self.left_child = None
+        self.left = None
+        self.right = None
+    
+    
+    def __str__(self):
+        return str(self.data)
 
         
+def in_order(root):
+    if root is None:
+        return
+    in_order(root.left)
+    print(root, end=', ')
+    in_order(root.right)
+    
+
 class Tree:
     def __init__(self):
-        self.root_node = None
+        self.root = None
+    
+    
+    def insert(self, data):
+        node = Node(data)
+        if self.root is None:
+            self.root = node
+        else:
+            current = self.root
+            while current:
+                if data < current.data:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = node
+                        break
+                else:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = node
+                        break
+    
+
+tree = Tree()
+tree.insert(5)
+tree.insert(2)
+tree.insert(1)
+tree.insert(6)
+tree.insert(7)
+tree.insert(3)
+tree.insert(10)
+tree.insert(5)
+in_order(tree.root)
+
+
