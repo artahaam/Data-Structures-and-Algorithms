1fe31ba4afb8ab4242d1f3b1daa85f252bc76ab6
alireza tahami <alireza.thm03@gmail.com>
2024-08-18 11:32:25 +0330

    search()

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -9,7 +9,7 @@ class Node:
         return str(self.data)
 
         
-def in_order(root):
+def in_order(root:Node):
     if root is None:
         return
     in_order(root.left)
@@ -28,7 +28,7 @@ class Tree:
             self.root = node
         else:
             current = self.root
-            while current:
+            while True:
                 if data < current.data:
                     if current.left:
                         current = current.left
@@ -41,17 +41,42 @@ class Tree:
                     else:
                         current.right = node
                         break
+                   
+                    
+    def search(self, data):
+        current = self.root
+        while True:
+            if data == current.data:
+                return True
+            if data < current.data:
+                current = current.left
+            else:
+                current = current.right
+
+            if current == None:
+                return False
     
+    
+                      
 
 tree = Tree()
 tree.insert(5)
-tree.insert(2)
 tree.insert(1)
+tree.insert(2)
+tree.insert(3)
+tree.insert(4)
 tree.insert(6)
 tree.insert(7)
-tree.insert(3)
+tree.insert(8)
+tree.insert(9)
 tree.insert(10)
-tree.insert(5)
 in_order(tree.root)
+print()
 
+print(tree.search(1))
+print(tree.search(2))
+print(tree.search(5))
+print(tree.search(10))
+print(tree.search(7))
+print(tree.search(100))
 
