b3803402c9800f0d98e40ed7f682c5ec5694730c
alireza tahami <alireza.thm03@gmail.com>
2024-08-20 23:18:08 +0330

    fixed indexing of delete_at() & add HeapSort -> sort()

--- Tree/MaxHeap.py
+++ Tree/MaxHeap.py
@@ -46,28 +46,54 @@ class MaxHeap:
     
     
     def delete_at(self, index):
+        index += 1
         item = self.heap[index]
         self.heap[index] = self.heap.pop()
         self.size -= 1
         self.sink(index)
         return item
         
+
+    def sort(self):
+        unsorted = self.heap.copy()
+        size = self.size
+        sorted_list = []
+        for i in range(self.size-1):
+            sorted_list.append(self.delete_root())
+        sorted_list.append(self.heap[-1])
+        self.heap = unsorted
+        self.size = size
+        return sorted_list
+    
     
     def display(self):
         print(self.heap[1:])
         
-        
+    
 if __name__ =='__main__':
     
     heap = MaxHeap()
     for i in (1, 4, 2, 6, 3, 9, 10, 5, 7, 8):
         heap.insert(i)
     heap.display()
+    
     heap.insert(11)
+    heap.display()
+
+    heap.insert(0)
     heap.display()
 
     heap.delete_root()
     heap.display()
 
     heap.delete_at(2)
+    heap.display()
+    
+    print(heap.sort())
+    heap.display()
+    
+    heap.delete_at(2)
+    heap.display()
+    
+    heap.delete_root()
     heap.display()
