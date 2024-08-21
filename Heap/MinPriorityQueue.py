887e03023a747351766826a037a8274c75e6c5b1
alireza tahami <alireza.thm03@gmail.com>
2024-08-21 12:45:42 +0330

    return value in extract_min()

--- Heap/MinPriorityQueue.py
+++ Heap/MinPriorityQueue.py
@@ -23,23 +23,43 @@ class MinPriorityQueue:
     
     
     def extract_min(self):
-        return self.heap.extract_min()
+        return self.objects[self.heap.extract_min()]
     
     
     def display(self):
         print(f'sort: {self.heap.sort()}')
         for key in self.heap.sort():
-            print(key, self.objects[key], end=', ')
+            print(key, self.objects[key])
         print()
 
 
-mpq = MinPriorityQueue()
+if __name__ == '__main__':
 
-mpq.instert(1, 'Reza')
-mpq.instert(3, 'Shervin')
-mpq.instert(2, 'Naser')
-mpq.instert(0, 'Maral')
-mpq.display()
+    mpq = MinPriorityQueue()
 
-print(mpq.extract_min())
+    mpq.instert(1, 'Reza')
+    mpq.instert(3, 'Shervin')
+    mpq.instert(2, 'Naser')
+    mpq.instert(0, 'Maral')
+    mpq.display()
+
+    for i, j in [
+                (66, 'Hasan Shamaeizade'),
+                (55, 'Ebrahim Raesi'),
+                (6, 'Mohsen Lorestani'),
+                (38, 'Mahmoud Ahmadinejad'),
+                (50, 'Siavash Ghomeishi'),
+                (31, 'Reza Ghoushannejad'),
+                (28, 'Mohsen Yegane'),
+                (49, 'Hashemi Rafsanjani'),
+                (93, 'Googoosh'),
+                (72, 'Ebi'),
+                ]:
         
+        mpq.instert(i, j)
+
+    mpq.display()
+        
+
+    print(mpq.extract_min())
+            
