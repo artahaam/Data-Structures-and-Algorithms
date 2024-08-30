7aa21b0e4748be4eee1a5158de0f979e482c48aa
alireza tahami <alireza.thm03@gmail.com>
2024-08-30 19:02:34 +0330

    change graph edges

--- Graph/Representations.py
+++ Graph/Representations.py
@@ -1,9 +1,10 @@
 # Adjacency lists
 graph = dict()
-graph['a'] = ['b', 'd']
-graph['b'] = ['a', 'c']
-graph['c'] = ['b', 'd']
-graph['d'] = ['a', 'c']
+graph['A'] = ['B', 'C']
+graph['B'] = ['E','C', 'A']
+graph['C'] = ['A', 'B', 'E','F']
+graph['E'] = ['B', 'C']
+graph['F'] = ['C']
 
 # Adjacency matrix
 elements = sorted(graph.keys())
@@ -16,11 +17,11 @@ for key in elements:
     for neighbor in graph[key]:
         edges.append((key, neighbor))
 
-
 for edge in edges:
     x = elements.index(edge[0])
     y = elements.index(edge[1])
     adj_matrix[x][y] = 1
+
 
 # Represent Graph with numbers instead of Letters
 adj = dict()
