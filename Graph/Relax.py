6f50832e6a8b84e8f08326b7c3bcf262adcdb410
alireza tahami <alireza.thm03@gmail.com>
2024-09-01 21:45:48 +0330

    add examples

--- Graph/Relax.py
+++ Graph/Relax.py
@@ -3,19 +3,77 @@
 # Instructors: Erik Demaine, Jason Ku, and Justin Solomon 
 # Recitation 11
 
-W2 = {
-    0 : {1 : 1, 3 : 2, 4 : -1}, #0
-    1 : {0 : 1},                #1
-    2 : {3 : 0},                #2
-    3 : {0 : 2, 2 : 0},         #3  
-    4 : {0 : -1},               #4
-    } 
+from Initialization import initialize
 
-def try_to_relax(adj, w, d, parent, u, v):
-    # extract weights
-    def w(u,v):
-        return W2[u][v]
-    
-    if d[v] > d[u] + w(u, v): # better path through vertex u
-        d[v] = d[u] + w(u, v) # relax edge with shorter path found
+
+def try_to_relax(w, d, parent, u, v):
+    if d[v] > d[u] + w[u][v]:       # better path through vertex u
+        d[v] = d[u] + w[u][v]       # relax edge with shorter path found
         parent[v] = u
+
+        
+if __name__ == '__main__':
+    
+    #---------------------EX.1----------------------------------------------------
+    print('\n' + ' '*10 + 'example 1 \n')
+    
+    # weights
+    W = {
+        0 : {1 : 1, 3 : 2, 4 : -1}, #0
+        1 : {0 : 1},                #1
+        2 : {3 : 0},                #2
+        3 : {0 : 2, 2 : 0},         #3  
+        4 : {0 : -1},               #4
+        } 
+    
+    # adjacency dictionary 
+    adj = dict()
+    for v in W.keys():
+        adj[v] = {u for u in W[v].keys()}
+    print('the adjacency dictionary: ')
+    print(adj)
+    print('-'*40 + '\n')
+
+    # initializing distance and parent lists
+    d, parent = initialize(adj, 0)
+    print(f'initialized distance -> {d} \ninitialized parent -> {parent}')
+    print('-'*40 + '\n') 
+
+    # relaxing all edges
+    for u in adj.keys():
+        for v in adj[u]:
+            try_to_relax(W, d, parent, u, v)
+    
+    print(f'relaxed distance -> {d}')
+    print('-'*80 + '\n')
+    
+    #---------------------EX.2----------------------------------------------------
+    print(' '*10 +'example 2 \n')
+        # weights
+    W = {
+        0: {1: -1, 2: 4}, #0
+        1: {2: 3, 3: 2, 4: 2},                #1
+        2: {},                #2
+        3: {1: 1, 2: 3},         #3  
+        4: {3: -3},               #4
+        } 
+    
+    # adjacency dictionary 
+    adj = dict()
+    for v in W.keys():
+        adj[v] = {u for u in W[v].keys()}
+    print(f'adjacency dictionary -> {adj} ')
+    print('-'*40 + '\n')
+
+    # initializing distance and parent lists
+    d, parent = initialize(adj, 0)
+    print(f'initialized distance -> {d} \ninitialized parent -> {parent}')
+    print('-'*40 + '\n') 
+
+    # relaxing all edges
+    for u in adj.keys():
+        for v in adj[u]:
+            try_to_relax(W, d, parent, u, v)
+    
+    print(f'relaxed distance -> {d}')
+    print('-'*40)
