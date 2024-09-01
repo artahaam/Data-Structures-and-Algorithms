f4f4761ed9bf0318ce53901806715ea458f2715c
alireza tahami <alireza.thm03@gmail.com>
2024-09-01 21:45:35 +0330

    comment

--- Graph/Initialization.py
+++ Graph/Initialization.py
@@ -1,6 +1,7 @@
+# the commenly used initialization in graph algorithms
 def initialize(adj,s):
-    infinity = float('inf') # number greater than sum of all + weights
-    d = [infinity for _ in adj] # shortest path estimates d(s, v)
-    parent = [None for _ in adj] # initialize parent pointers
-    d[s], parent[s] = 0, s # initialize source
+    infinity = float('inf')         # number greater than sum of all + weights
+    d = [infinity for _ in adj]     # shortest path estimates d(s, v)
+    parent = [None for _ in adj]    # initialize parent pointers
+    d[s], parent[s] = 0, s          # initialize source
     return d, parent
